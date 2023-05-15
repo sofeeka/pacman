9454521559b88ece3658d611398eca8c9255053b
Andriy <agrychukh@gmail.com>
2023-05-16 01:24:33 +0300

    Running game via invokeLater()

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,36 @@
 import javax.swing.*;
 
+class GameRunner extends Thread
+{
+    private int x;
+    private int y;
+
+    GameRunner( int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void run()
+    {
+        GameModel gameModel = new GameModel(x, y);
+        GameView gameView = new GameView(gameModel);
+        gameModel.setGameView( gameView );
+        GameController gameController = new GameController(gameModel, gameView );
+    }
+}
+
 public class Game
 {
-
     Game()
     {
-
     }
 
     public void startNewGame(int x, int y)
     {
-        GameModel gameModel = new GameModel(x, y);
-
-        GameView gameView = new GameView(gameModel);
-
-        gameModel.setGameView( gameView );
-
-        gameView.renderModel();
-        GameController gameController = new GameController(gameModel, gameView );
-        //frame
-//        view
+        GameRunner runner = new GameRunner(x, y);
+        SwingUtilities.invokeLater( runner );
     }
 
 }
