b46da9d778c98eae5f6ea131feec131f14d3c24d
Andriy <agrychukh@gmail.com>
2023-05-16 01:37:09 +0300

    Changed ghost rendering to icons array

--- src/GameView_Ghost.java
+++ src/GameView_Ghost.java
@@ -1,51 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class GameView_Ghost
 {
     GameView gameView;
-    ImageIcon fullSizeIconOpen;
-    ImageIcon fullSizeIconClosed;
-    ImageIcon renderingIconOpen;
-    ImageIcon renderingIconClosed;
-    private boolean open;
+    ArrayList<ImageIcon> fullSizeIcons;
+    ArrayList<ImageIcon> renderingIcons;
+    private int iconIndex;
     int width;
     int height;
 
     GameView_Ghost(GameView gameView)
     {
         this.gameView = gameView;
-        fullSizeIconOpen = new ImageIcon("images\\ghost1.png");
-        fullSizeIconClosed = new ImageIcon("images\\ghost2.png");
-        open = true;
+
+        fullSizeIcons = new ArrayList<>();
+        fullSizeIcons.add(new ImageIcon("images\\ghost1.png"));
+        fullSizeIcons.add(new ImageIcon("images\\ghost2.png"));
+
+        iconIndex = 0;
     }
     public void render(Component c, Graphics g, Rectangle cellRect)
     {
-        if(this.width != cellRect.width || this.height != cellRect.height || renderingIconOpen == null) // todo check direction
+        if(this.width != cellRect.width || this.height != cellRect.height || renderingIcons == null) // todo check direction
         {
-            Image open = fullSizeIconOpen.getImage();
-            Image resizedOpen = open.getScaledInstance(cellRect.width, cellRect.height, Image.SCALE_SMOOTH);
-            renderingIconOpen = new ImageIcon(resizedOpen);
+            renderingIcons = new ArrayList<>();
 
-            Image closed = fullSizeIconClosed.getImage();
-            Image resizedClosed = closed.getScaledInstance(cellRect.width, cellRect.height, Image.SCALE_SMOOTH);
-            renderingIconClosed = new ImageIcon(resizedClosed);
+            for( ImageIcon icon : fullSizeIcons )
+            {
+                Image fullSizeImage = icon.getImage();
+                Image renderingImage = fullSizeImage.getScaledInstance(cellRect.width, cellRect.height, Image.SCALE_SMOOTH);
+                renderingIcons.add( new ImageIcon( renderingImage ) );
+            }
         }
 
         int x = cellRect.x;
         int y = cellRect.y;
 
         getRenderingIcon().paintIcon(c, g, x, y);
-
     }
 
     public void changeIcon()
     {
-        open = !open;
+        iconIndex++;
+        if( iconIndex >= fullSizeIcons.size())
+            iconIndex = 0;
+
         gameView.renderModel();
     }
     private ImageIcon getRenderingIcon() {
-        return open ? renderingIconOpen : renderingIconClosed;
+        return this.renderingIcons.get(iconIndex);
     }
 
 }
